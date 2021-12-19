806628eceb04df2c6306dca5e15f6c001adb1862
Rina Moonlight <barabanova018@yandex.ru>
2021-12-19 16:36:57 +0400

    Исправила Health bar

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,8 +8,6 @@ public class Enemy : Entity
     public Transform playerTransform;
     public Transform thisTransform;
 
-    public float Hitpoints;
-    public float MaxHitpoints = 5;
     public HealthBarEnemy Healthbar;
 
     public enum EnemyType
@@ -19,17 +17,18 @@ public class Enemy : Entity
     };
     protected override void Start()
     {
-        Hitpoints = MaxHitpoints;
-        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
-
         base.Start();
         playerTransform = FindObjectOfType<Player>().gameObject.transform;
         thisTransform = GetComponent<Transform>();
+
+        stats.Health = stats.MaxHealth;
+        Healthbar.SetHealth(stats.Health, stats.MaxHealth);
     }
 
     public override void GetDamage(float x)
     {
         base.GetDamage(x);
+        Healthbar.SetHealth(stats.Health, stats.MaxHealth);
     }
     protected void FixedUpdate()
     {
