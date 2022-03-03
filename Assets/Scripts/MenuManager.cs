b1bd4cf05fcbb6824c35d24780a39cf8a2af6e2a
Rina Moonlight <barabanova018@yandex.ru>
2022-03-03 09:12:24 +0400

    Сделала переход

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,12 +5,53 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     public void OnQuit()
     {
        Application.Quit();
     }
     public void OnPlay()
     {
-        SceneManager.LoadScene(1);
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel() 
+    {
+        StartCoroutine(LoadLevel(1)); 
+    }
+
+    //IEnumerator LoadLevel(int levelIndex)
+    //{
+    //    transition.SetTrigger("Start");
+
+    //    yield return new WaitForSeconds(transitionTime);
+
+    //    SceneManager.LoadScene(levelIndex);
+    //}
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelIndex);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress >= .9f && !asyncLoad.allowSceneActivation)
+            {
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+                }
+            }
+
+            else if (asyncLoad.progress >= .8f)
+            {
+                transition.SetTrigger("Start");
+            }
+            yield return null;
+        }
     }
 }
